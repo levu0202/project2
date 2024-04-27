3a5c41156b0d997a6e8b64ea62c1169ff1caf5c0
levu0202 <levu0202@gmail.com>
2024-04-26 21:59:42 -0400

    new

--- client.c
+++ client.c
@@ -14,12 +14,20 @@ void error(const char * msg) {
 }
 
 
+void print_binary(unsigned char byte) {
+    for (int i = 7; i >= 0; i--) {
+        printf("%c", (byte & (1 << i)) ? '1' : '0');
+    }
+}
+
+
 int main(int argc, char * argv[]) {
     int sockfd, portNum, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
+    uint32_t fileSize;
 
-    char buffer[256];
+    char *buffer;
     if (argc < 3) {
        fprintf(stderr,"usage %s hostname port\n", argv[0]);
        exit(0);
@@ -35,26 +43,69 @@ int main(int argc, char * argv[]) {
         exit(0);
     }
 
-    bzero((char *) &serv_addr, sizeof(serv_addr));
+    memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+    memcpy(&serv_addr.sin_addr.s_addr, server->h_addr, server->h_length);
     serv_addr.sin_port = htons(portNum);
 
     if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
 
-    strcpy(buffer, "qr_code.png"); 
+    FILE *file = fopen("qr_code.png", "rb");
+    if (file == NULL) error("Error opening file");
+    fseek(file, 0, SEEK_END);
+    fileSize = ftell(file);
+    rewind(file);
 
-    n = write(sockfd, buffer, strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer, 256);
-    n = read(sockfd, buffer, 255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("Server reply: %s\n", buffer);
+    buffer = malloc(fileSize + 1);
+    if (buffer == NULL) error("Memory allocation failed");
+    
+    
+    size_t bytesRead = fread(buffer, 1, fileSize, file);
+    if (bytesRead != fileSize)
+        error("Error reading file");
+    // printf("Binary representation of the file:\n");
+    // for (size_t i = 0; i < fileSize; i++) {
+    //     print_binary(buffer[i]);
+    //     printf(" ");  // Space between bytes for readability
+    //     if ((i + 1) % 8 == 0) printf("\n");  // Newline every 8 bytes for better readability
+    // }
+    // printf("\n");
 
+    uint32_t netFileSize = htonl(fileSize);
+    if (write(sockfd, &netFileSize, sizeof(netFileSize)) < 0) 
+        error("ERROR writing file size to socket");
+
+    if (write(sockfd, buffer, fileSize) < 0) 
+         error("ERROR writing file content to socket");
+    // buffer[fileSize] = '\0'; // Null-terminate the string
+    // printf("Received filename: %s\n", buffer);
+    free(buffer);  // Free the buffer after it's sent
+    fclose(file);  // Close the file as soon as it's no longer needed
+
+    // Read the response code and URL length
+    uint32_t responseCode, urlLength;
+    if (read(sockfd, &responseCode, sizeof(responseCode)) < 0)
+        error("ERROR reading response code from socket");
+    responseCode = ntohl(responseCode);
+
+    if (read(sockfd, &urlLength, sizeof(urlLength)) < 0)
+        error("ERROR reading URL length from socket");
+    urlLength = ntohl(urlLength);
+
+    char *message = malloc(urlLength + 1); // Allocate buffer for the received URL/message
+    if (message == NULL)
+        error("Memory allocation failed");
+
+    if (read(sockfd, message, urlLength) < 0) {
+        free(message);
+        error("ERROR reading URL from socket");
+    }
+    message[urlLength] = '\0'; // Null-terminate the string
+    printf("Response code: %d\n", responseCode);
+    printf("url length: %d\n", urlLength);
+    printf("Received message: %s\n", message);
+    free(message);
     close(sockfd);
     return 0;
 }
--- server.c
+++ server.c
@@ -13,12 +13,15 @@ void error(const char *msg) {
     exit(1);
 }
 
+
+
 int main(int argc, char *argv[]) {
     int sockfd, newsockfd, portNum;
     socklen_t clilen;
-    char buffer[1024];
+    char *buffer;
     struct sockaddr_in server_addr, client_addr;
-    int n;
+    unsigned int fileSize;
+
 
     if (argc < 2) {
         fprintf(stderr,"ERROR, no port provided\n");
@@ -30,7 +33,7 @@ int main(int argc, char *argv[]) {
         perror("Error opening socket");
     }
 
-    bzero((char *) &server_addr, sizeof(server_addr));
+    memset(&server_addr, 0, sizeof(server_addr));
     portNum = atoi(argv[1]);
 
     server_addr.sin_family = AF_INET;
@@ -46,29 +49,59 @@ int main(int argc, char *argv[]) {
     if (newsockfd < 0) 
         error("ERROR on accept");
 
-    bzero(buffer, 1024);
-    n = read(newsockfd, buffer, 1023);
-    if (n < 0) error("ERROR reading from socket");
+    
+    int n = read(newsockfd, &fileSize, sizeof(fileSize));
+    if (n < 0) {
+        free(buffer);
+        error("ERROR reading from socket");
 
+    } 
+    fileSize = ntohl(fileSize); 
+
+    buffer = malloc(fileSize + 1);
+    if (buffer == NULL) error("Memory allocation failed");
+    n = recv(newsockfd, buffer, fileSize, MSG_WAITALL);
+    if (n < fileSize) {
+        free(buffer);
+        error("ERROR reading full data from socket");
+    }
+    // printf("Binary data received:\n");
+    // for (unsigned int i = 0; i < fileSize; i++) {
+    //     print_binary(buffer[i]);
+    //     printf(" ");  // Add space between bytes
+    //     if ((i + 1) % 8 == 0) printf("\n");  // New line every 8 bytes
+    // }
+
+    FILE *file = fopen("received_qr_code.png", "wb");
+    if (file == NULL) {
+        free(buffer);
+        error("Failed to open file for writing");
+    }
+    fwrite(buffer, 1, fileSize, file);
+    fclose(file);
+    free(buffer);
+
+    // Execute ZXing command
     char command[512];
-    sprintf(command, "java -cp javase.jar:core.jar com.google.zxing.client.j2se.CommandLineRunner %s", buffer);
-
+    sprintf(command, "java -cp javase.jar:core.jar com.google.zxing.client.j2se.CommandLineRunner received_qr_code.png");
     FILE *fp = popen(command, "r");
     if (fp == NULL) {
         error("Failed to run command");
     }
-    int count = 0;
-    while (fgets(buffer, 1023, fp) != NULL) {
-        count ++;
-        if (count == 3) {
-            n = write(newsockfd, buffer, strlen(buffer));
-            if (n < 0) {
-                perror("ERROR writing to socket");
-                break;
-        }
+    char resultBuffer[1024];
+    int lineCount = 0;
+    while (fgets(resultBuffer, sizeof(resultBuffer), fp) != NULL) {
+        lineCount++;
+        if (lineCount == 3) {
+            uint32_t responseCode = htonl(0); // Assume success response code for this example
+            uint32_t urlLength = htonl(strlen(resultBuffer));
+            printf("%s", resultBuffer);
+            write(newsockfd, &responseCode, sizeof(responseCode));
+            write(newsockfd, &urlLength, sizeof(urlLength));
+            write(newsockfd, resultBuffer, strlen(resultBuffer));
+            break; // Stop after processing the third line
         }
     }
-
     pclose(fp);
 
     close(newsockfd);
